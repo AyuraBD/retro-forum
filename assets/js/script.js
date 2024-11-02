552a3a57f99322d44c0540406c3f0f9fa1d5abe0
Mohammad Ayub <5043ayub@gmail.com>
2024-11-02 23:53:29 +0600

    Join our Forum and some javascript with API

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,21 @@ const loadPosts = async () =>{
 }
 
 const displayPosts = (posts) =>{
-
+	console.log(posts);
     const postContainer = document.getElementById('post-container');
     posts.forEach(post => {
-			const {category, comment_count, description, image, isActive, title, author , posted_time, view_count} = post;
-        console.log(post);
-        const div = document.createElement('div');
+			const {id, category, comment_count, description, image, isActive, title, author , posted_time, view_count} = post;
+			const div = document.createElement('div');
         div.innerHTML = `
           <div class="mb-4 bg-[#797DFC1A] rounded-3xl lg:p-10 md:p-5 sm:p-3 single-post flex justify-start gap-5">
 						<div class="indicator">
-							<span class="indicator-item badge badge-success">
+							<span id="indicator" class="indicator-item badge ${isActive ? 'badge-success' : 'badge-warning'}">
 							</span>
 							<div class="rounded-2xl bg-white grid h-32 w-32 place-items-center">
 								
 							</div>
 						</div>
-						<div>
+						<div class="w-full">
 							<div class="tags text-[#12132DCC] text-2xl font-bold] mb-4">
 								<span class="mr-5"># ${category}</span>
 								<span>Author : ${author.name}</span>
@@ -38,14 +37,52 @@ const displayPosts = (posts) =>{
 									<span>${posted_time}</span>
 								</div>
 								<div>
-									<img src="assets/images/envelop.svg" alt="">
+									<button onclick="postDetails(${id})"><img src="assets/images/envelop.svg" alt=""></button>
 								</div>
 							</div>
 						</div>
 					</div>
         `;
-				postContainer.appendChild(div);
+		postContainer.appendChild(div);
     })
 }
+
+
+
+
+const postDetails = async (id)=>{
+	try{
+		const url = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts/`);
+		if(!url.ok) throw new Error(`Error: ${url.status}`); 
+		const res = await url.json();
+		
+		const post = res.posts.find(post => post.id === id);
+		const {title, view_count} = post;
+		const rightRead = document.getElementById('right-read-section');
+		const div = document.createElement('div');
+		div.classList.add('mb-4', 'bg-white', 'p-3', 'rounded-md', 'flex', 'justify-between', 'items-center');
+		div.innerHTML = `
+			<p>${title}</p>
+			<div class="flex justify-center items-center">
+				<img class="mr-2" src="assets/images/eye.svg" alt="">
+				<span>${view_count}</span>
+			</div>
+		`
+		rightRead.appendChild(div);
+
+		const readPost = document.getElementById('read-post');
+		const rightReadSection = document.getElementById('right-read-section');
+		readPost.innerText = rightReadSection.childElementCount;
+	}
+	catch (error){
+		console.error('Failed to  fetch', error)
+	}
+};
+
+
+// const postDetailsShow = (data) =>{
+// 	const {id, category} = data;
+// 	console.log(id);
+// }
 
 loadPosts()
